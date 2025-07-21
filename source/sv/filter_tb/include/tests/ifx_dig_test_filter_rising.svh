c561576ab9c0c80ce2ecf3a2522e45f50594b401
tudor0grigore <tudor0511@gmail.com>
2025-07-21 12:30:16 +0300

    day6

--- source/sv/filter_tb/include/tests/ifx_dig_test_filter_rising.svh
+++ source/sv/filter_tb/include/tests/ifx_dig_test_filter_rising.svh
@@ -95,6 +95,10 @@ class ifx_dig_test_filter_rising extends ifx_dig_testbase;
 
         end
 
+        `TEST_INFO("\n\n\nPrinting Coverage results\n\n\n")
+        `TEST_INFO($sformatf("\ncg_filter_ctrl coverage is = %f\n", dig_env.scoreboard.cg_filter_ctrl.get_coverage()))
+        `TEST_INFO($sformatf("\ncg_int_status_read coverage is = %f\n", dig_env.scoreboard.cg_int_status_read.get_coverage()))
+
 
         phase.drop_objection(this);
     endtask
